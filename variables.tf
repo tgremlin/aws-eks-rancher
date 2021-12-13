c9c19d21894c7a2425a4b27147cc3fbf0ab690ad
Allan Tenison <atenison@bokf.com>
2021-12-13 12:48:44 -0600

    Added eks module

--- variables.tf
+++ variables.tf
@@ -15,3 +15,44 @@ variable pub_subnets {
   default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   description = "CIDRs for the public subnets"
 }
+
+variable lt_ng_desired_capacity {
+  type        = string
+  default     = 3
+  description = "Desired number of instances in EKS Nodegroup launch template"
+}
+
+variable lt_ng_max_capacity {
+  type        = string
+  default     = 10
+  description = "Max number of instances in EKS Nodegroup launch template"
+}
+
+variable lt_ng_min_capacity {
+  type        = string
+  default     = 3
+  description = "Minimum number of instances in EKS Nodegroup launch template"
+}
+
+variable lt_ng_disk_size {
+  type        = string
+  default     = 20
+  description = "EKS Nodegroup instance disk size"
+}
+
+variable lt_ng_disk_type {
+  type        = string
+  default     = "gp3"
+  description = "EKS Nodegroup instance disk type"
+}
+
+variable lt_ng_instance_type {
+  type        = string
+  default     = ["t3.micro"]
+  description = "EC2 Instance type for EKS Nodegroup"
+}
+
+
+
+
+
